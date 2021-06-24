da01f41c9b89b9ed00a88d7e1119efb40647815d
DavidRMorphew <morphewd@umich.edu>
2021-06-24 14:41:19 -0400

    building out reducer and action to fetch sets from API

--- src/reducers/legoSetReducer.js
+++ src/reducers/legoSetReducer.js
@@ -1,6 +1,8 @@
 
 export const legoSetReducer = (state = [], action) => {
     switch (action.type){
+        case 'ADD_SETS':
+            return action.payload
         default:
             return state
     }
