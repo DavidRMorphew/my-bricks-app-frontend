ed51a9233a167509c64796402a6c2d34470ca747
DavidRMorphew <morphewd@umich.edu>
2021-06-25 19:37:47 -0400

    styling for font and background updated

--- src/components/LegoSetCard.js
+++ src/components/LegoSetCard.js
@@ -4,7 +4,10 @@ const LegoSetCard = props => {
     const set = props.legoSet
     return(
         <div>
-        <Card border="dark" className="my-4" style={{ width: '18rem' }}>
+        <Card border="dark" className="my-4" style={{ 
+            width: '18rem',
+            color: 'black' 
+            }}>
             <Card.Header as="h4">{set.name}</Card.Header>
             <Card.Img src={set.imageUrl}/>
                 <Card.Body>
--- src/containers/LegoSetsContainer.js
+++ src/containers/LegoSetsContainer.js
@@ -12,14 +12,8 @@ class LegoSetsContainer extends Component{
     }
 
     handleLegoSetLoading = () => {
-        return (this.props.loading) ? <p>LOADING...</p> : <CardDeck>{this.renderLegoSetCards()}</CardDeck>
+        return (this.props.loading) ? <h4 className="over-background">LOADING...</h4> : <CardDeck>{this.renderLegoSetCards()}</CardDeck>
     }
-
-    // renderLegoListTest = () => {
-    //     const renderArray = this.props.legoSets.map((set, index) => (<li key={set.id}><p>{index+1}.) {set.name}</p><img src={set.imageUrl}/></li>))
-    //     console.log(renderArray)
-    //     return renderArray
-    // }
 
     renderLegoSetCards = () => (
         this.props.legoSets.map(set => <LegoSetCard key={set.id} legoSet={set}/>)
@@ -28,7 +22,7 @@ class LegoSetsContainer extends Component{
     render(){
         return(
             <Container fluid className="container">
-                <h1>Lego Sets!</h1>
+                <h1 className="over-background">Lego Sets!</h1>
                     {this.handleLegoSetLoading()}
             </Container>
         )
