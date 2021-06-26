7b6e99641f34295983b6ae8ef4f3050171d12d50
DavidRMorphew <morphewd@umich.edu>
2021-06-25 21:02:19 -0400

    FilterInputComponent converted to class component

--- src/components/FilterInputComponent.js
+++ src/components/FilterInputComponent.js
@@ -1,9 +1,19 @@
-const FilterInputComponent = () => {
-    return(
-        <div>
-            <h1 className="over-background">Filter Results Here!</h1>
-        </div>
-    )
+import React, {Component} from "react"
+
+class FilterInputComponent extends Component {
+    
+    state = {
+        
+    }
+
+    render(){
+        return(
+            <div>
+                <h1 className="over-background">Filter Results Here!</h1>
+                
+            </div>
+        )
+    }
 }
 
 export default FilterInputComponent
