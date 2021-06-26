ce2976fdc83e7b35b26c244e3a99ad89ec4d538d
DavidRMorphew <morphewd@umich.edu>
2021-06-25 21:18:01 -0400

    controlled form added to FilterInputComponent with button that clears the search bar

--- src/components/FilterInputComponent.js
+++ src/components/FilterInputComponent.js
@@ -3,14 +3,30 @@ import React, {Component} from "react"
 class FilterInputComponent extends Component {
     
     state = {
-        
+        filterTerm: ''
+    }
+
+    handleOnChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleOnClick = e => {
+        e.preventDefault()
+        this.setState({
+            filterTerm: ''
+        })
     }
 
     render(){
         return(
             <div>
                 <h1 className="over-background">Filter Results Here!</h1>
-                
+                <form>
+                    <input type="text" name="filterTerm" onChange={this.handleOnChange} value={this.state.filterTerm}/>
+                    <button onClick={this.handleOnClick}>Clear Search</button>
+                </form>
             </div>
         )
     }
