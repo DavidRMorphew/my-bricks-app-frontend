d45574b05aa6daaf335e5e380346248864f0790b
DavidRMorphew <morphewd@umich.edu>
2021-06-27 13:12:08 -0400

    legoSetShowdisplay data being built out

--- src/components/LegoSetShowDisplay.js
+++ src/components/LegoSetShowDisplay.js
@@ -2,6 +2,11 @@ import Card from 'react-bootstrap/Card'
 
 const LegoSetShowDisplay = props => {
     const {set} = props
+
+    const renderOwnedValue = (set) => {
+        return !!set.owned ? "Owned" : "Not Owned"
+    }
+
     return(
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <Card 
@@ -9,7 +14,31 @@ const LegoSetShowDisplay = props => {
                 className="my-4"
                 style={{ width: '80%' }}
             >
-                {set.name}
+                <Card.Header as="h1">{set.name}</Card.Header>
+                <Card.Img src={set.imageUrl}/>
+                    <Card.Body>
+                    <Card.Text as="h2">
+                            Year: <strong>{set.year}</strong>
+                    </Card.Text>
+                    <Card.Text as="h2">
+                        Theme: <strong>{set.themeName}</strong>
+                    </Card.Text>
+                    <Card.Text as="h2">
+                        Building Instructions: 
+                    </Card.Text>   
+                    <Card.Text as="h2">
+                        Total Bricks: <strong>{set.totalBricks}</strong>
+                    </Card.Text>
+                    <Card.Text as="h2">
+                        Owned: <strong>{renderOwnedValue(set)}</strong>
+                    </Card.Text>
+                            
+                            
+                         
+                            
+                        
+                        
+                    </Card.Body>
             </Card>
         </div>
     )
