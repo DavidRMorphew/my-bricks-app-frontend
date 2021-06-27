96c0f8a76266195585513ed6441dc4a0de518cc2
DavidRMorphew <morphewd@umich.edu>
2021-06-27 18:22:23 -0400

    building out fetchSetPartSpecs action in setPartSpecsActions

--- src/actions/setPartSpecsActions.js
+++ src/actions/setPartSpecsActions.js
@@ -1,9 +1,15 @@
-const baseUrl = "http://localhost:3001"
+const baseUrl = "http://localhost:3001/lego_sets"
 
-export const fetchSetPartSpecs = () => {
+export const addSetPartSpecs = (setPartSpecs) => ({type: 'ADD_SET_PART_SPECS', payload: setPartSpecs})
+
+export const fetchSetPartSpecs = (setId) => {
     return (dispatch) => {
         dispatch({type: 'LOADING'})
-        
+        fetch(`${baseUrl}/${setId}/set_part_specs`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
 
         dispatch({type: 'LOADING_COMPLETE'})
     }
--- src/reducers/setPartSpecsReducer.js
+++ src/reducers/setPartSpecsReducer.js
@@ -1,5 +1,7 @@
 export const setPartSpecsReducer = (state = [], action) => {
     switch (action.type){
+        case 'ADD_SET_PART_SPECS':
+            return [...state, ...action.payload]
         default:
             return state
     }
