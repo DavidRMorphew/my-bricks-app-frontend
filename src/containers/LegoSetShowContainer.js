b06c7d8ccc17c4baa81d645aebe29fb13b6dc6ea
DavidRMorphew <morphewd@umich.edu>
2021-06-28 18:27:44 -0400

    fetchPartsOfSet action creator moved into findLegoSet of LegoSetShowContainer and extraneous mounting code for fetches removed

--- src/containers/LegoSetShowContainer.js
+++ src/containers/LegoSetShowContainer.js
@@ -8,26 +8,6 @@ import NotFoundErrorDisplay from '../components/NotFoundErrorDisplay'
 
 class LegoSetShowContainer extends Component {
     
-    // componentDidMount(){
-    //     this.props.fetchPartsOfSet(this.props.routeInfo.match.params.id)
-    //     // this.props.fetchSetPartSpecs(legoSetId)
-    // }
-
-    // componentDidUpdate(prevProps){
-    //     if (this.props.routeInfo.match.params.id !== prevProps.routeInfo.match.params.id){
-    //         debugger
-    //         this.props.fetchPartsOfSet(this.props.routeInfo.match.params.id)
-    //     }
-    // }
-    
-    // shouldComponentUpdate(nextProps){
-    //     if (this.props.routeInfo.match.params.id !== nextProps.routeInfo.match.params.id){
-    //         return true
-    //     } else {
-    //         return false
-    //     }
-    // }
-
     findLegoSet = legoSetId => {
         const legoSet = this.props.legoSets.find(set => set.id === legoSetId)
         if (legoSetId && legoSet) {
