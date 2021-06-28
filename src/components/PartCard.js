bc6a02a825dd3934eb56ad393e0fc6c4aa4cc5cb
DavidRMorphew <morphewd@umich.edu>
2021-06-28 16:54:31 -0400

    PartCard elements displayed updated

--- src/components/PartCard.js
+++ src/components/PartCard.js
@@ -6,13 +6,19 @@ const PartCard = props => {
         <div>
             
         <Card border="dark" className="my-4" style={{ 
-            width: '18rem',
+            width: '12rem',
             color: 'black' 
             }}>
+            <Card.Header as="h4">Part Number: {part.partNumber}</Card.Header>
             <Card.Img src={part.imageUrl}/>
             <Card.Body>
                 <Card.Text>
-                    {props.partQuantity}
+                    <strong>Part Name: </strong>{part.name}
+                <br></br>
+                    <strong>Part Color: </strong>{part.color}                    
+                </Card.Text>
+                <Card.Text>
+                    <strong>Number in Set: </strong>{props.partQuantity}
                 </Card.Text>
             </Card.Body>
         </Card>
