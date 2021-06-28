0fa0997d05a477738b05182cc7f8b520ca3c8fea
DavidRMorphew <morphewd@umich.edu>
2021-06-28 08:07:08 -0400

    legoSetActions basUrl expanded to include '/lego_sets' and commented-out code removed

--- src/actions/legoSetActions.js
+++ src/actions/legoSetActions.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3001"
+const baseUrl = "http://localhost:3001/lego_sets"
 
 export const addSets = (sets) => ({type: 'ADD_SETS', payload: sets})
 
@@ -7,14 +7,10 @@ export const toggleOwned = (set) => ({type: 'TOGGLED_OWNED_VALUE', payload: set}
 export const fetchLegoSets = () => {
     return (dispatch) => {
         dispatch({type: 'LOADING'})
-        fetch(`${baseUrl}/lego_sets`)
+        fetch(baseUrl)
         .then(resp => resp.json())
         .then(data => {
             const sets = data.data.map(legoSet => legoSet.attributes) // Madeline Stark said this is OK.
-            // dispatch({
-            //     type: 'ADD_SETS', 
-            //     payload: sets
-            // })
             dispatch(addSets(sets))
             dispatch({type: 'LOADING_COMPLETE'})
         })
@@ -33,7 +29,7 @@ export const changeOwnedSetStatus = (id) => {
             body: JSON.stringify(id)
         }
 
-        fetch(`${baseUrl}/lego_sets/${id}`, configObj)
+        fetch(`${baseUrl}/${id}`, configObj)
         .then(resp => resp.json())
         .then(data => {
             const set = data.data.attributes
