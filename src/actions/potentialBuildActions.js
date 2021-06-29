3675da128933eb60fd9e155f46a58c780a8f221f
DavidRMorphew <morphewd@umich.edu>
2021-06-29 14:13:12 -0400

    building out fetchPotentialBuilds action creator

--- src/actions/potentialBuildActions.js
+++ src/actions/potentialBuildActions.js
@@ -5,6 +5,10 @@ export const addPotentialBuilds = (searchResults) => ({type: 'ADD_POTENTIAL_BUIL
 export const fetchPotentialBuilds = (strictParam) => {
     return (dispatch) => {
         dispatch({type: 'LOADING'})
-        
+        fetch(`${baseUrl}/${strictParam}`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data.data)
+        })
     }
 }
