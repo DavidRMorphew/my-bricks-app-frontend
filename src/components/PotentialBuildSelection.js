6f7f724c7251f78daec7b397b47e6a6b7e82ef44
DavidRMorphew <morphewd@umich.edu>
2021-06-29 15:18:40 -0400

    PotentialBuildsSelection accordion and collapsible card added with editing of instructions and details

--- src/components/PotentialBuildSelection.js
+++ src/components/PotentialBuildSelection.js
@@ -3,6 +3,8 @@ import { fetchPotentialBuilds } from "../actions/potentialBuildActions"
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 
 const PotentialBuildSelection = props => {
     return(
@@ -14,26 +16,38 @@ const PotentialBuildSelection = props => {
                 <br></br>
             <h4>We'll find all the sets in our database that you can build based on all of the parts you already own.</h4>
                 <br></br>
-            <h1>HOW IT WORKS:</h1>
-            <h4>Lego sets' parts are filtered for matching, excluding: </h4>
-                <h5>Minifigures and Figures,</h5> <h5>Accessories,</h5> <h5>Non-lego parts</h5>
-                <br></br>
-            <h4>Then we add up how many you have of each part and how many you need for each set to determine matches.</h4>
-                <br></br>
             <h1>Please Choose Below:</h1>
-            <h4>You have the option of finding matches regardless of part colors (Option 1) or with strict part matching by color (Option 2)</h4>
             <Row>
                 <Col>
-                    <h2>Option 1 (Regardless of Color)</h2>
-                    <button onClick={() => props.fetchPotentialBuilds("notStrict")}>Find Potential Builds</button>
+                    <button onClick={() => props.fetchPotentialBuilds("notStrict")}>Find Potential Builds (Color Substitutions)</button>
+                    {/* <h2>Option 1 (Regardless of Color)</h2> */}
                 </Col>
                 <Col>
-                    <h2>Option 2 (Strict Color Matching)</h2>
-                    <h3></h3>
                     <button onClick={() => props.fetchPotentialBuilds("strict")}>Find Potential Builds (strict color matching)</button>
+                    {/* <h2>Option 2 (Strict Color Matching)</h2> */}
                 </Col>
             </Row>
+            {/* <h4>You have the option of finding matches regardless of part colors (Option 1) or with strict part matching by color (Option 2)</h4> */}
             <br></br>
+            <Accordion>
+                <Card style={{backgroundColor: "rgba(0,0,0,0)"}}>
+                    <Card.Header>
+                        <Accordion.Toggle eventKey="0">
+                            <h4>CLICK HERE TO READ HOW IT WORKS</h4>
+                            
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                            <h4>Lego sets' parts are filtered for matching, excluding: </h4>
+                                <h5>Minifigures and Figures,</h5> <h5>Accessories,</h5> <h5>Non-lego parts</h5>
+                                <br></br>
+                            <h4>Then we add up how many you have of each part and how many you need for each set to determine matches.</h4>
+                                <br></br>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
         </Container>
     )
 }
