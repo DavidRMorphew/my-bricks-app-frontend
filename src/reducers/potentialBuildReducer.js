dce5ce674733f068d3f6d8163dfae6af88c67371
DavidRMorphew <morphewd@umich.edu>
2021-06-29 18:19:32 -0400

    toggleOwned action creator action resets the store state of potentialBuilds as well as updating the owned status of a set

--- src/reducers/potentialBuildReducer.js
+++ src/reducers/potentialBuildReducer.js
@@ -2,6 +2,8 @@ const potentialBuildReducer = (state = [], action) => {
     switch (action.type){
         case "ADD_POTENTIAL_BUILDS":
             return action.payload
+        case 'TOGGLED_OWNED_VALUE':
+            return []
         default:
             return state
     }
