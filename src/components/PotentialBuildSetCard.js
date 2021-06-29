63c813469e92c4c91240eb694d6452b6cda0f306
DavidRMorphew <morphewd@umich.edu>
2021-06-29 17:57:44 -0400

    PotentialBuildSetCard displays in CardDeck properly for results of either search

--- src/components/PotentialBuildSetCard.js
+++ src/components/PotentialBuildSetCard.js
@@ -1,6 +1,30 @@
+import Card from 'react-bootstrap/Card'
+import { Link } from "react-router-dom";
+
 const PotentialBuildSetCard = props => {
+    const {set} = props
+
     return(
-        <div>Card</div>
+        <div>
+            <Card border="dark" className="my-4" style={{ 
+            width: '18rem',
+            color: 'black' 
+            }}>
+            <Card.Header as="h4"><Link to={`/lego_sets/${set.id}`}>{set.name}</Link></Card.Header>
+            <Card.Img src={set.imageUrl}/>
+                <Card.Body>
+                    <Card.Title><strong>{set.setNumber}</strong></Card.Title>
+                        <Card.Text>
+                            Year: <strong>{set.year}</strong>
+                            <br></br>
+                            Theme: <strong>{set.themeName}</strong>
+                            <br></br>
+                            Total Bricks: <strong>{set.totalBricks}</strong>
+                            <br></br>
+                        </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
