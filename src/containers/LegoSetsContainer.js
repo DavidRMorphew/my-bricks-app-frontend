46c95ac4e8c0ae3467023a1dd722d6477068528a
DavidRMorphew <morphewd@umich.edu>
2021-07-01 13:12:14 -0400

    filterTerm variable assignment added with destructuring of this.state in LegoSetsContainer

--- src/containers/LegoSetsContainer.js
+++ src/containers/LegoSetsContainer.js
@@ -10,7 +10,7 @@ import {
   } from "react-router-dom";
 
 class LegoSetsContainer extends Component{
-
+    
     state = {
         filterTerm: ''
     }
@@ -31,24 +31,25 @@ class LegoSetsContainer extends Component{
     }
 
     render(){
+        const { filterTerm } = this.state
         return(
             <Switch>
                 <Route exact path="/lego_sets">
                     <FilterInputComponent 
-                        handleOnChange={this.handleFilterInputOnChange} 
-                        handleOnClick={this.handleFilterInputClearOnClick} 
-                        value={this.state.filterTerm}
+                        handleOnChange={ this.handleFilterInputOnChange } 
+                        handleOnClick={ this.handleFilterInputClearOnClick } 
+                        value={ filterTerm }
                     />
-                    <LegoSetCards filterTerm={this.state.filterTerm}/>
+                    <LegoSetCards filterTerm={ filterTerm }/>
                 </Route>
                 <Route exact path="/lego_sets/owned">
-                    <LegoSetCards subSetTerm={"owned"}/>
+                    <LegoSetCards subSetTerm={ "owned" }/>
                 </Route>
                 <Route exact path="/lego_sets/potential_builds">
                      <PotentialBuildSelection />
                      <PotentialBuildSetsResults />
                 </Route>
-                <Route path="/lego_sets/:id" component={routeInfo => <LegoSetShowContainer routeInfo={routeInfo} />}/>
+                <Route path="/lego_sets/:id" component={ routeInfo => <LegoSetShowContainer routeInfo={ routeInfo } /> }/>
             </Switch>
         )
 
