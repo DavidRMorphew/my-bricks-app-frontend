f0fc2879060b88b875767bf4739d2432a30bc807
DavidRMorphew <morphewd@umich.edu>
2021-07-01 15:58:17 -0400

    converted LegoSetCards from class to functional component and assigned variables by destructuring props

--- src/components/LegoSetCards.js
+++ src/components/LegoSetCards.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import LegoSetCard from '../components/LegoSetCard'
 import Container from 'react-bootstrap/Container'
 import CardDeck from 'react-bootstrap/CardDeck'
 import { connect } from 'react-redux'
 import { changeOwnedSetStatus } from '../actions/legoSetActions'
 
-class LegoSetCards extends Component {
+const LegoSetCards = props =>{
+    
+    const { legoSets, loading, changeOwnedSetStatus } = props
   
-    handleLegoSetLoading = () => {
-        return (this.props.loading) ? <h4 className="over-background">LOADING...</h4> : <CardDeck>{this.renderLegoSetCards()}</CardDeck>
+    const handleLegoSetLoading = () => {
+        return (loading) ? <h4 className="over-background">LOADING...</h4> : <CardDeck>{renderLegoSetCards()}</CardDeck>
     }
 
-    renderLegoSetCards = () => (
-        this.props.legoSets.map(set => <LegoSetCard key={set.id} legoSet={set} changeOwnedSetStatus={this.props.changeOwnedSetStatus}/>)
+    const renderLegoSetCards = () => (
+        legoSets.map(set => <LegoSetCard key={set.id} legoSet={set} changeOwnedSetStatus={changeOwnedSetStatus}/>)
     )
-    render(){
-        
+           
     return(
         <Container fluid className="container">
-                <h1 className="over-background">Lego Sets: {this.props.legoSets.length}</h1>
-                    {this.handleLegoSetLoading()}
+                <h1 className="over-background">Lego Sets: {legoSets.length}</h1>
+                    {handleLegoSetLoading()}
         </Container>
     )
-    }
 }
 
 const mapStateToProps = (state, ownProps) => {
