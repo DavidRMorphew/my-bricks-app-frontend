fff1a7388297d3cedf7251f6d84acb294bcadf14
DavidRMorphew <morphewd@umich.edu>
2021-07-02 06:23:24 -0400

    old notes removed from reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,3 @@
-// combine reducers
-// legoSets
-//  actions: ADD_OWNED, FETCH_SETS
-// parts
-//  actions: FETCH_PARTS_OF_SET, PARTS_OWNED
-// themes
-//  actions: FETCH_THEMES
 import { combineReducers } from 'redux'
 import legoSetReducer from './legoSetReducer'
 import loadingReducer from './loadingReducer'
