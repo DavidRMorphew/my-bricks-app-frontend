5bcccd23bb4c1fc58a671329873c3437f5ada62d
DavidRMorphew <morphewd@umich.edu>
2021-07-03 20:19:23 -0400

    built basic noteReducer and added to combineReducers' rootReducer for notes state in store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,13 @@ import loadingReducer from './loadingReducer'
 import setPartSpecsReducer from './setPartSpecsReducer'
 import partReducer from './partReducer'
 import potentialBuildReducer from './potentialBuildReducer'
+import noteReducer from './noteReducer'
 
 export const rootReducer = combineReducers({
     legoSets: legoSetReducer,
     loading: loadingReducer,
     setPartSpecs: setPartSpecsReducer,
     parts: partReducer,
-    potentialBuilds: potentialBuildReducer
+    potentialBuilds: potentialBuildReducer,
+    notes: noteReducer
 })
---
+++ src/reducers/noteReducer.js
@@ -0,0 +1,8 @@
+const noteReducer = (state = [], action) => {
+    switch (action.type){
+        default:
+            return state
+    }
+}
+
+export default noteReducer
