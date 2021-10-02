b049cb85cb70ed6c67a55b1f24d3084a515e0dbb
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-02 18:27:56 -0400

    add registerUser action

---
+++ src/actions/userActions.js
@@ -0,0 +1,16 @@
+export const registerUser = user => {
+    const url = 'http://localhost:3001/users'
+    return (dispatch) => {
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accepts": "application/json"
+            },
+            body: JSON.stringify(user)
+        }
+        fetch(url, configObj)
+        .then(resp => resp.json())
+        .then(userData => console.log(userData))
+    }
+}
