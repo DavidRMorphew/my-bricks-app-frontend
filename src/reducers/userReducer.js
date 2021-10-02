98eb16c4996b00ad5d00e7187c6ecac179261056
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-02 18:29:04 -0400

    add set user action case to userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,6 +2,8 @@ const userReducer = (state = {}, action) => {
     switch(action.type){
         case 'REMOVE_USER':
             return {}
+        case 'SET_USER':
+            return user
         default:
             return state
     }
