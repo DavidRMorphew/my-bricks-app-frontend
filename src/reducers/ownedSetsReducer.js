20d6cfddae7c1412c7dc23d7271ecf63fefe90e7
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-12 19:56:02 -0400

    add case of add owned sets and correct export name

--- src/reducers/ownedSetsReducer.js
+++ src/reducers/ownedSetsReducer.js
@@ -1,8 +1,10 @@
 const ownedSetsReducer = (state = [], action) => {
     switch (action.type){
+        case 'ADD_OWNED_SETS':
+            return action.payload
         default:
             return state
     }
 }
 
-export default ownedSetReducer
+export default ownedSetsReducer
