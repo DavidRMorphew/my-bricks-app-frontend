cd6f9d056d7f705aa5a7130f9173814ba1feed25
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-12 21:41:11 -0400

    add fetchUserOwnedSets action creator with nested url

--- src/actions/ownedPartsActions.js
+++ src/actions/ownedPartsActions.js
@@ -1,1 +1,13 @@
+const baseUrl = "http://localhost:3001/lego_sets/owned_sets"
+
 export const addOwnedSets = (ownedSets) => ({type: 'ADD_OWNED_SETS', payload: ownedSets})
+
+export const fetchUserOwnedSets = () => {
+    return (dispatch) => {
+        fetch(baseUrl)
+        .then(resp => resp.json())
+        .then(ownedSetsData => {
+            console.log(ownedSetsData)
+        })
+    }
+}
