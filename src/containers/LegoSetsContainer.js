69dcf52bd052623fae005459bcf2e43b5073e567
DRM <60860458+DavidRMorphew@users.noreply.github.com>
2023-06-07 14:49:20 -0400

    Update to react 18 (#7)
    * Update to React 18
    
    * Remove yarn lock
    
    * Update main rendering for React 18
    
    * Implement useDeferredValue for the lego set search results
    
    * Update packages

--- src/containers/LegoSetsContainer.js
+++ src/containers/LegoSetsContainer.js
@@ -1,47 +1,50 @@
-import React from 'react'
-import { useState } from 'react'
-import FilterInputComponent from '../components/FilterInputComponent'
-import LegoSetCards from '../components/LegoSetCards'
-import LegoSetShowContainer from './LegoSetShowContainer'
-import PotentialBuildSelection from '../components/PotentialBuildSelection'
-import PotentialBuildSetsResults from '../components/PotentialBuildSetsResults'
-import {
-    Switch,
-    Route
-  } from "react-router-dom";
+import React, { useDeferredValue } from "react";
+import { useState } from "react";
+import FilterInputComponent from "../components/FilterInputComponent";
+import LegoSetCards from "../components/LegoSetCards";
+import LegoSetShowContainer from "./LegoSetShowContainer";
+import PotentialBuildSelection from "../components/PotentialBuildSelection";
+import PotentialBuildSetsResults from "../components/PotentialBuildSetsResults";
+import { Switch, Route } from "react-router-dom";
 
 const LegoSetsContainer = () => {
+  const [filterTerm, setFilterTerm] = useState("");
 
-    const [filterTerm, setFilterTerm] = useState('')
+  const deferredFilterTerm = useDeferredValue(filterTerm);
 
-    const handleFilterInputOnChange = e => {
-        setFilterTerm(e.target.value)
-    }
+  const handleFilterInputOnChange = (e) => {
+    setFilterTerm(e.target.value);
+  };
 
-    const handleFilterInputClearOnClick = e => {
-        setFilterTerm('')
-    }
+  const handleFilterInputClearOnClick = (e) => {
+    setFilterTerm("");
+  };
 
-    return(
-        <Switch>
-            <Route exact path="/lego_sets">
-                <FilterInputComponent 
-                    handleOnChange={ handleFilterInputOnChange } 
-                    handleOnClick={ handleFilterInputClearOnClick } 
-                    value={ filterTerm }
-                />
-                <LegoSetCards filterTerm={ filterTerm }/>
-            </Route>
-            <Route exact path="/lego_sets/owned">
-                <LegoSetCards subSetTerm={ "owned" }/>
-            </Route>
-            <Route exact path="/lego_sets/potential_builds">
-                    <PotentialBuildSelection />
-                    <PotentialBuildSetsResults />
-            </Route>
-            <Route path="/lego_sets/:id" component={ routeInfo => <LegoSetShowContainer routeInfo={ routeInfo } /> }/>
-        </Switch>
-    )
-}
+  return (
+    <Switch>
+      <Route exact path="/lego_sets">
+        <FilterInputComponent
+          handleOnChange={handleFilterInputOnChange}
+          handleOnClick={handleFilterInputClearOnClick}
+          value={filterTerm}
+        />
+        <LegoSetCards filterTerm={deferredFilterTerm} />
+      </Route>
+      <Route exact path="/lego_sets/owned">
+        <LegoSetCards subSetTerm={"owned"} />
+      </Route>
+      <Route exact path="/lego_sets/potential_builds">
+        <PotentialBuildSelection />
+        <PotentialBuildSetsResults />
+      </Route>
+      <Route
+        path="/lego_sets/:id"
+        component={(routeInfo) => (
+          <LegoSetShowContainer routeInfo={routeInfo} />
+        )}
+      />
+    </Switch>
+  );
+};
 
-export default LegoSetsContainer
+export default LegoSetsContainer;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { rootReducer } from './reducers/index';
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from "react-redux";
+import { legacy_createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./reducers/index";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-ReactDOM.render(
+const store = legacy_createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
