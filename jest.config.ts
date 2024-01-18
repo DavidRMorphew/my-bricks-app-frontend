72931a1dd82fc8628298dcb5390ccdc637d5a972
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2024-01-18 17:15:20 -0500

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+import type { Config } from "jest";
+const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
-  coverageDirectory: "coverage",
-  setupFilesAfterEnv: ["src/setupTests.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
 };
+
+export default config;
