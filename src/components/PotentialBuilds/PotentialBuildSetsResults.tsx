ea4442a533d23f87ad24d98b392880e6f0c4dd0c
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2024-01-27 17:56:47 -0500

    Correct type imports in PotentialBuildSetsResults and set up filter functionality for results

--- src/components/PotentialBuilds/PotentialBuildSetsResults.tsx
+++ src/components/PotentialBuilds/PotentialBuildSetsResults.tsx
@@ -4,6 +4,14 @@ import Loading from "../Shared/Loading";
 import PotentialBuildSetCard from "./PotentialBuildSetCard";
 import { connect } from "react-redux";
 import type { LegoSet } from "../LegoSets/types";
+import FilterInputComponent from "../FilterInputComponent";
+import {
+  type ChangeEventHandler,
+  type MouseEventHandler,
+  useCallback,
+  useDeferredValue,
+  useState,
+} from "react";
 
 interface PotentialBuildSetsResultsProps {
   loading: boolean;
@@ -14,9 +22,34 @@ const PotentialBuildSetsResults = ({
   loading,
   potentialBuilds,
 }: PotentialBuildSetsResultsProps) => {
-  const handleLegoSetLoading = () => {
-    if (loading) {
-      return (
+  const [filterTerm, setFilterTerm] = useState("");
+
+  const deferredFilterTerm = useDeferredValue(filterTerm);
+
+  const handleFilterInputOnChange: ChangeEventHandler<HTMLInputElement> =
+    useCallback(
+      (e) => {
+        setFilterTerm(e.target.value);
+      },
+      [setFilterTerm]
+    );
+
+  const handleFilterInputClearOnClick: MouseEventHandler<HTMLButtonElement> =
+    useCallback(() => {
+      setFilterTerm("");
+    }, [setFilterTerm]);
+
+  const filteredResults = potentialBuilds.filter((set: LegoSet) => {
+    const regex = new RegExp(deferredFilterTerm as string, "i");
+    return (
+      regex.test(set.name) ||
+      regex.test(set.themeName) ||
+      regex.test(set.setNumber)
+    );
+  });
+  return (
+    <Container fluid className="container">
+      {loading ? (
         <>
           <Loading />
           <h1 className="over-background">
@@ -24,30 +57,28 @@ const PotentialBuildSetsResults = ({
             Please do not change owned sets while we search.
           </h1>
         </>
-      );
-    } else if (potentialBuilds.length) {
-      return (
-        <div>
-          <h1 className="over-background">
-            Potential Build Results: {potentialBuilds.length}
-          </h1>
-          <CardDeck>{renderPotentialBuildCards()}</CardDeck>
-        </div>
-      );
-    }
-  };
-
-  const renderPotentialBuildCards = () =>
-    potentialBuilds.map((potBuildSet) => (
-      <PotentialBuildSetCard
-        key={`Pot-build-${potBuildSet.id}`}
-        set={potBuildSet}
-      />
-    ));
-
-  return (
-    <Container fluid className="container">
-      {handleLegoSetLoading()}
+      ) : (
+        filteredResults?.length && (
+          <div>
+            <h1 className="over-background">
+              Potential Build Results: {filteredResults.length}
+            </h1>
+            <FilterInputComponent
+              handleOnChange={handleFilterInputOnChange}
+              handleOnClick={handleFilterInputClearOnClick}
+              value={filterTerm}
+            />
+            <CardDeck>
+              {filteredResults.map((potBuildSet) => (
+                <PotentialBuildSetCard
+                  key={`Pot-build-${potBuildSet.id}`}
+                  set={potBuildSet}
+                />
+              ))}
+            </CardDeck>
+          </div>
+        )
+      )}
     </Container>
   );
 };
